9e2494ee2d4ddfdc90344420f3001bfe17b63427
DoludaSergey <dalass@ukr.net>
2013-07-01 19:46:43 +0300

    first project version

---
+++ EmpirMet_laba4/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace EmpirMet_laba4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btn_DoIt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Результат первого метода";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(246, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Результат второго метода";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(4, 45);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(215, 77);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(249, 45);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(215, 77);
+            this.textBox2.TabIndex = 3;
+            // 
+            // btn_DoIt
+            // 
+            this.btn_DoIt.Location = new System.Drawing.Point(193, 141);
+            this.btn_DoIt.Name = "btn_DoIt";
+            this.btn_DoIt.Size = new System.Drawing.Size(75, 23);
+            this.btn_DoIt.TabIndex = 4;
+            this.btn_DoIt.Text = "Посчитать";
+            this.btn_DoIt.UseVisualStyleBackColor = true;
+            this.btn_DoIt.Click += new System.EventHandler(this.btn_DoIt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 179);
+            this.Controls.Add(this.btn_DoIt);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Решение систем лин. уравнений Developed by Doluda S.V.";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btn_DoIt;
+    }
+}
+
---
+++ EmpirMet_laba4/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EmpirMet_laba4
+{
+    public partial class Form1 : Form
+    {
+
+        //матрица первой системы   
+        double[,] A = {
+                            {3,2,2},
+                            {1,-2,5},
+                            {-2,-3,4}
+                        };
+        public static int n = 3;
+        int r = 0, s = 0;
+        //матрица для сохранения первой системы
+        double[,] Save ={
+                             {3,2,2},
+                            {1,-2,5},
+                            {-2,-3,4}
+                        };
+        //матрица для сохранения второй системы
+        double[,] Save2 ={
+                             {3,2,2,1},
+                            {1,-2,5,5},
+                            {-2,-3,4,3}
+                        };
+        //матрица второй системы
+        double[,] A2 ={
+                             {3,2,2,1},
+                            {1,-2,5,5},
+                            {-2,-3,4,3}
+                        };
+
+        
+        public Form1()
+        {                       
+            InitializeComponent();
+        }
+
+        public void First(ref double[,] A, ref double rasr, ref int r, ref int s)
+        {
+            while (s != n)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if ((i != r) && (j != s))
+                            A[i, j] = Save[i, j] - (Save[i, s] * Save[r, j]) / Save[r, s];
+                        if (j == s)
+                            A[i, j] = Save[i, j] / rasr;
+                        if (i == r)
+                            A[i, j] = Save[i, j] / (-rasr);
+                    }
+                }
+                A[r, s] = 1 / rasr;
+                SaveMatrix(ref A);
+                r++; s++;
+                if (r == n)
+                    break;
+                rasr = A[r, s];
+            }
+
+            double res1 = A[0, 0] * 1 + A[0, 1] * 5 + A[0, 2] * 3;
+            double res2 = A[1, 0] * 1 + A[1, 1] * 5 + A[1, 2] * 3;
+            double res3 = A[2, 0] * 1 + A[2, 1] * 5 + A[2, 2] * 3;
+            textBox1.AppendText("Result 1= " + res1 + "\r\n" + "Result 2= " + res2 + "\r\n" + "Result 3= " + res3 + "\r\n");
+        }
+
+        public void Second(ref double[,] A2, ref double rasr, ref int r, ref int s)
+        {
+            while (s != n)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < 2; j++)
+                    {
+                        if ((i != r) && (j != s))
+                            A2[i, j] = Save2[i, j] - (Save2[i, s] * Save2[r, j]) / Save2[r, s];
+                        if (j == s)
+                            A2[i, j] = Save2[i, j] / rasr;
+                        if (i == r)
+                            A2[i, j] = Save2[i, j] / (-rasr);
+                    }
+                }
+                A2[r, s] = 1 / rasr;
+                SaveMatrix2(ref A2);
+                r++; s++;
+                if (r == n)
+                    break;
+                rasr = A2[r, s];
+            }
+            double res21 = A2[0, 3];
+            double res22 = A2[1, 3];
+            double res23 = A2[2, 3];
+            textBox2.AppendText("Result 1_sec= " + res21 + "\r\n" + "Result 2_sec= " + res22 + "\r\n" + "Result 3_sec= " + res23 + "\r\n");
+        }
+        //сохраняем матрицы для работы с текущей и предыдущей
+        public void SaveMatrix(ref double[,] M)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Save[i, j] = M[i, j];
+                }
+            }
+        }
+        public void SaveMatrix2(ref double[,] M)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Save2[i, j] = M[i, j];
+                }
+            }
+        }
+
+
+        private void btn_DoIt_Click(object sender, EventArgs e)
+        {
+            double rasr = A[r, s];
+            //считаем по первому методу
+            First(ref A, ref rasr, ref r, ref s);
+            r = 0; s = 0;
+            rasr = A2[r, s];
+            //считаем по второму методу
+            Second(ref A2, ref rasr, ref r, ref s);
+
+        }
+    }
+}
